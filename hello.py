b16b9d6a4e53ee524a195527e3f4c2e037483711
action-hong <kkopitehong@gmail.com>
2016-09-14 00:11:20 +0800

    到jinja2模板的bootstrap处

--- hello.py
+++ hello.py
@@ -8,43 +8,28 @@
 __author__ = 'kkopite'
 
 
-from flask import Flask
+from flask import Flask,render_template
 from flask import request
 from flask import abort
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-	# user_agent = request.headers.get('User-Agent')
-	# return '<h1>your browser is %s</h1>' % user_agent
-
-	# response = make_response('<h1>This document carries a cookie!</h1>')
-	# response.set_cookie('answer','42')
-	# return response
+	return render_template('index.html',test='<h1>test</h1>')
 	
-	return redirect('http://www.example.com/') #重定向
-	
-def load_user(id):
-	pass
-
-
-@app.route('/user/<int:id>')
-def get_user(id):
-	#从数据库获取user,没有就返回404
-	user = load_user(id)
-	if not user:
-		abort(404)
-	return '<h1>Hello,%s!</h1>' % name
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello,%s!</h1>' % name
+	#前面那个name是占位符,在模板中使用
+	return render_template('user.html',name=name)
 
 
 
 if __name__ == '__main__':
-	# app.run()
-	manager.run()   #使用命令行来决定服务器的启动方式
+	app.run(debug=True)
+	# manager.run()   #使用命令行来决定服务器的启动方式
